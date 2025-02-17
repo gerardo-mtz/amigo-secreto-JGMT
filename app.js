dd3be5eeeec3f7411e2b4ca66b5298208d431010
pepe-mtz <gerardo.mtz.t@gmail.com>
2025-02-17 17:20:43 -0600

    Se agrego la funcion para añadir amigos a la lista

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaDeAmigos = [];
+let listaAmigos = [];
+let campoAmigo = document.getElementById("amigo");
+let ulListaAmigos = document.getElementById("listaAmigos");
 
-let campoAmigos = document.getElementById("amigo");
-
-if (campoAmigos === null) {
+// Check if the input field 'campoAmigo' is null. If so, alert the user to insert a name.
+// Otherwise, retrieve the value from the input field, add it to 'listaAmigos', and clear the input field.
+if (campoAmigo === null) {
     alert("Por favor, inserte un nombre");
 } else {
-    let nombreAmigo = campoAmigos.value;
-    listaDeAmigos.push(nombreAmigo);
-    campoAmigos.value = "";
+    let nombreAmigo = campoAmigo.value;
+    listaAmigos.push(nombreAmigo);
+    campoAmigo.value = "";
 }
 
-listaAmigos.innerHTML = "";
+//Adds the value from the input field 'campoAmigo' to the 'listaAmigos' array.
+//It retrieves the current value from the input field and appends it to the list.
+
+function agregarAmigo() {
+    listaAmigos.push(campoAmigo.value);
+
+}
+    listaAmigos.innerHTML = "";
+    
+
+
 
