7c8db3d5c1b94ee3ca29102582e19c664d6116fd
pepe-mtz <gerardo.mtz.t@gmail.com>
2025-02-20 17:50:43 -0600

    Version final del challenge

--- app.js
+++ app.js
@@ -6,23 +6,22 @@ let campoAmigo = document.getElementById("amigo");
 let ulListaAmigos = document.getElementById("listaAmigos");
 let ulResultado = document.getElementById("resultado");
 
-// Revisa si el campo de amigo esta vacio y alerta al usuario de ser asi
+//Revisa si el campo de amigo esta vacio y alerta al usuario de ser asi
 //Si el campo de amigo no esta vacio, agrega el amigo a la lista 'listaAmigos' y limpia el campo
-if (campoAmigo.value === "") {
-    alert("Por favor, inserte un nombre");
-} else {
-    let nombreAmigo = campoAmigo.value;
-    listaAmigos.push(nombreAmigo);
-    campoAmigo.value = "";
-}
-
-//Esta funcion agrega un amigo a la lista 'listaAmigos' y lo muestra en la lista de amigos
+//Muestra el valor agregado a la lista de amigos
 function agregarAmigo() {
-    listaAmigos.push(campoAmigo.value);
-    ulListaAmigos.innerHTML += `<li>${campoAmigo.value}</li>`;
+    if (campoAmigo.value === "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        let nombreAmigo = campoAmigo.value;
+        listaAmigos.push(nombreAmigo);
+        ulListaAmigos.innerHTML += `<li>${nombreAmigo}</li>`;
+        campoAmigo.value = "";
+    }
 }
 
-// Esta funcion elige un amigo aleatorio de la lista 'listaAmigos' y muestra el resultado
+// Esta funcion elige un amigo aleatorio de la lista 'listaAmigos'
+// Muestra el resultado del sorteo de la lista
 function sortearAmigo() {
     let amigoSecreto = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
     ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
